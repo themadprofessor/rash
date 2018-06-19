a9c306ecbdf4ed85d23fab2bd1992f9a10f8ddd5
Stuart Reilly <stu@reilly-family.co.uk>
2018-06-19 11:50:40 +0100

    Migrate from rust-crypto to smaller crates

--- src/main.rs
+++ src/main.rs
@@ -1,31 +1,44 @@
 #[macro_use] extern crate clap;
 extern crate failure;
-extern crate crypto;
+extern crate md5;
+extern crate blake2;
+extern crate ripemd160;
+extern crate digest;
+extern crate sha1;
+extern crate whirlpool;
+extern crate sha2;
+extern crate sha3;
+extern crate generic_array;
 
 use failure::Error;
 use clap::{Arg, SubCommand, AppSettings, ArgMatches};
-use crypto::digest::Digest;
+use digest::{Digest, VariableOutput};
 
 use std::io::{Read, BufReader};
 use std::fs::File;
 
 fn run() -> Result<(), Error> {
     let matches = args();
-    let mut alg: Box<Digest> = get_alg(&matches)?;
-
     let mut file = BufReader::new(File::open(matches.value_of("FILE").ok_or_else(|| failure::err_msg("missing file"))?)
         .map_err(Error::from)?);
-    let mut buf = [0 as u8; 1024];
-    loop {
-        if file.read(&mut buf)? == 0 {
-            break;
-        } else {
-            alg.input(&buf);
-        }
-    }
-    println!("{}", alg.result_str());
+    let hash = get_alg(&matches, &mut file)?;
+
+    println!("{}", hash);
 
     Ok(())
+}
+
+fn calc_hash<D, R>(mut digest: D, input: &mut R) -> Result<String, Error> where D: Digest, R: Read {
+    let mut buf = [0 as u8; 1024];
+    loop {
+        if input.read(&mut buf).map_err(Error::from)? == 0 {
+            break;
+        } else {
+            digest.input(&buf);
+        }
+    };
+
+    Ok(digest.result().iter().map(|x| format!("{:02x}", x)).collect::<String>())
 }
 
 fn args<'a>() -> ArgMatches<'a> {
@@ -47,16 +60,16 @@ fn args<'a>() -> ArgMatches<'a> {
                 .long("length")
                 .help("Length of output hash in bytes")
                 .long_help("Length of output hash in bytes. Must be between 1 & 64 inclusive.")
-                .required(true)))
+                .default_value("64")))
         .subcommand(SubCommand::with_name("blake2s")
             .about("BLAKE2s algorithm")
-            .long_about("BLAKE2s algorithm. Recommended over BLAKE2b on 64bit platforms.")
+            .long_about("BLAKE2s algorithm. Recommended over BLAKE2b on 32bit platforms.")
             .arg(Arg::with_name("len")
                 .short("l")
                 .long("length")
                 .help("Length of output hash in bytes")
                 .long_help("Length of output hash in bytes. Must be between 1 & 32 inclusive.")
-                .required(true)))
+                .default_value("32")))
         .subcommand(SubCommand::with_name("sha2")
             .about("SHA2 algorithms")
             .long_about("SHA2 Algorithms. Supports SHA256 (truncated to 224, 256) and SHA512 (truncated to 224, 256, 384, 512).")
@@ -87,7 +100,6 @@ fn args<'a>() -> ArgMatches<'a> {
                 .long_help("Length of the output hash. Supported lengths with algorithms:\
                 \n\talg:\tlen
                 \n\tsha3:\t244, 256, 384, 512\
-                \n\tshake:\t128, 256\
                 \n\tkeccak:\t244, 256, 384, 512\n")
                 .possible_values(&["128", "244", "256", "384", "512"])
                 .takes_value(true)
@@ -98,47 +110,42 @@ fn args<'a>() -> ArgMatches<'a> {
                 .help("SHA3 Algorithm")
                 .long_help("The SHA3 algorithm to use. If not given, sha3 is assumed. See len's help for length algorithm combinations.")
                 .takes_value(true)
-                .possible_values(&["sha3", "shake", "keccak"])))
+                .possible_values(&["sha3", "keccak"])
+                .default_value("sha3")))
         .arg(Arg::with_name("FILE")
             .help("File to calculate the hash of")
             .global(true))
         .get_matches()
 }
 
-fn get_alg<'a>(matches: &ArgMatches<'a>) -> Result<Box<Digest>, Error> {
+fn get_alg<'a, R>(matches: &ArgMatches<'a>, input: &mut R) -> Result<String, Error> where R: Read {
     match matches.subcommand() {
-        ("md5", _) => Ok(Box::new(crypto::md5::Md5::new())),
-        ("whirlpool", _) => Ok(Box::new(crypto::whirlpool::Whirlpool::new())),
-        ("sha1", _) => Ok(Box::new(crypto::sha1::Sha1::new())),
-        ("ripemd160", _) => Ok(Box::new(crypto::ripemd160::Ripemd160::new())),
+        ("md5", _) => calc_hash(md5::Md5::new(), input),
+        ("whirlpool", _) => calc_hash(whirlpool::Whirlpool::new(), input),
+        ("sha1", _) => calc_hash(sha1::Sha1::new(), input),
+        ("ripemd160", _) => calc_hash(ripemd160::Ripemd160::new(), input),
         ("blake2b", Some(matches)) => {
             let len = matches.value_of("len").unwrap().parse().map_err(Error::from)?;
-            if len < 1 || len > 64 {
-                Err(failure::err_msg("len must be between 1 & 64 inclusive"))
-            } else {
-                Ok(Box::new(crypto::blake2b::Blake2b::new(len)))
-            }
+            calc_hash(<blake2::Blake2b as VariableOutput>::new(len)
+                          .map_err(|_| failure::err_msg("invalid length"))?, input)
         },
         ("blake2s", Some(matches)) => {
             let len = matches.value_of("len").unwrap().parse().map_err(Error::from)?;
-            if len < 1 || len > 32 {
-                Err(failure::err_msg("len must be between 1 & 64 inclusive"))
-            } else {
-                Ok(Box::new(crypto::blake2s::Blake2s::new(len)))
-            }
+            calc_hash(<blake2::Blake2s  as VariableOutput>::new(len)
+                          .map_err(|_| failure::err_msg("invalid length"))?, input)
         },
         ("sha2", Some(matches)) => {
             match matches.value_of("alg").unwrap().parse().map_err(Error::from)? {
                 256 => match matches.value_of("len").unwrap().parse().map_err(Error::from)? {
-                    224 => Ok(Box::new(crypto::sha2::Sha224::new())),
-                    256 => Ok(Box::new(crypto::sha2::Sha256::new())),
+                    224 => calc_hash(sha2::Sha224::new(), input),
+                    256 => calc_hash(sha2::Sha256::new(), input),
                     _ => Err(failure::err_msg("invalid length for SHA2-256"))
                 },
                 512 => match matches.value_of("len").unwrap().parse().map_err(Error::from)? {
-                    224 => Ok(Box::new(crypto::sha2::Sha512Trunc224::new())),
-                    256 => Ok(Box::new(crypto::sha2::Sha512Trunc256::new())),
-                    384 => Ok(Box::new(crypto::sha2::Sha384::new())),
-                    512 => Ok(Box::new(crypto::sha2::Sha512::new())),
+                    224 => calc_hash(sha2::Sha512Trunc224::new(), input),
+                    256 => calc_hash(sha2::Sha512Trunc256::new(), input),
+                    384 => calc_hash(sha2::Sha384::new(), input),
+                    512 => calc_hash(sha2::Sha512::new(), input),
                     _ => Err(failure::err_msg("invalid length for SHA2-256"))
                 },
                 _ => Err(failure::err_msg("invalid SHA2 algorithm"))
@@ -147,22 +154,17 @@ fn get_alg<'a>(matches: &ArgMatches<'a>) -> Result<Box<Digest>, Error> {
         ("sha3", Some(matches)) => {
             match matches.value_of("alg").unwrap_or_else(|| "sha3") {
                 "sha3" => match matches.value_of("len").unwrap().parse().map_err(Error::from)? {
-                    224 => Ok(Box::new(crypto::sha3::Sha3::sha3_224())),
-                    256 => Ok(Box::new(crypto::sha3::Sha3::sha3_256())),
-                    384 => Ok(Box::new(crypto::sha3::Sha3::sha3_384())),
-                    512 => Ok(Box::new(crypto::sha3::Sha3::sha3_512())),
+                    224 => calc_hash(sha3::Sha3_224::new(), input),
+                    256 => calc_hash(sha3::Sha3_256::new(), input),
+                    384 => calc_hash(sha3::Sha3_384::new(), input),
+                    512 => calc_hash(sha3::Sha3_512::new(), input),
                     _ => Err(failure::err_msg("invalid length for SHA3"))
                 },
-                "shake" => match matches.value_of("len").unwrap().parse().map_err(Error::from)? {
-                    128 => Ok(Box::new(crypto::sha3::Sha3::shake_128())),
-                    256 => Ok(Box::new(crypto::sha3::Sha3::shake_256())),
-                    _ => Err(failure::err_msg("invalid length for SHAKE"))
-                },
                 "keccak" => match matches.value_of("len").unwrap().parse().map_err(Error::from)? {
-                    224 => Ok(Box::new(crypto::sha3::Sha3::keccak224())),
-                    256 => Ok(Box::new(crypto::sha3::Sha3::keccak256())),
-                    384 => Ok(Box::new(crypto::sha3::Sha3::keccak384())),
-                    512 => Ok(Box::new(crypto::sha3::Sha3::keccak512())),
+                    224 => calc_hash(sha3::Keccak224::new(), input),
+                    256 => calc_hash(sha3::Keccak256::new(), input),
+                    384 => calc_hash(sha3::Keccak384::new(), input),
+                    512 => calc_hash(sha3::Keccak512::new(), input),
                     _ => Err(failure::err_msg("invalid length for Keccak"))
                 }
                 _ => Err(failure::err_msg("invalid SHA3 algorithm"))
